153d1ef9c4c9f77890c50db0d0fce6b9e2a92541
celeste-liogier <cc@epinomis.fr>
2020-11-04 16:16:16 +0100

    Update main.cpp

--- test github/main.cpp
+++ test github/main.cpp
@@ -9,7 +9,8 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // je teste des trucs
+    //blahblahhhhh
     std::cout << "Hello, World!\n";
     return 0;
 }
