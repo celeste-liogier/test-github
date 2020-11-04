06984f8b7465c7f5af68113b95a066380c27fc52
celeste-liogier <cc@epinomis.fr>
2020-11-04 16:00:38 +0100

    Initial Commit

---
+++ test github/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test github
+//
+//  Created by Céleste Liogier on 04/11/2020.
+//  Copyright © 2020 Céleste Liogier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
